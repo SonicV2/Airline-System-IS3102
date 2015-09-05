a4bc7871a101ac52db5705810d88a61f390c8422
word3play <word3play@hotmail.com>
2015-09-05 13:52:47 +0800

    Rectified APS entity

---
+++ AirlineSystem-ejb/src/java/APS/Route.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package APS.Entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Family
+ */
+@Entity
+public class Route implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long routeId;
+    private String origin;
+    private String destination;
+    @OneToMany(cascade={CascadeType.ALL}, mappedBy="route")
+    private List<Flight> flights = new ArrayList<Flight>();
+    
+
+    public Long getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(Long routeId) {
+        this.routeId = routeId;
+    }
+    
+    public String getOrigin() {
+        return origin;
+    }
+    
+    public void setOrigin(String origin){
+        this.origin = origin;
+    }
+    
+    public String getDestination() {
+        return destination;
+    }
+    
+    public void setDestination(String destination){
+        this.destination = destination;
+    }
+    
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(List<Flight> flights) {
+        this.flights = flights;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (routeId != null ? routeId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Route)) {
+            return false;
+        }
+        Route other = (Route) object;
+        if ((this.routeId == null && other.routeId != null) || (this.routeId != null && !this.routeId.equals(other.routeId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "APS.Entity.Route[ id=" + routeId + " ]";
+    }
+    
+}
