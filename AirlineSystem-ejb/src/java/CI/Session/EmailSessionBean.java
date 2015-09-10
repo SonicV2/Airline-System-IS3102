d395a571a722e19490c9af3138859bbf7304babf
smu <smu@192.168.1.1>
2015-09-10 17:11:36 +0800

    Forget Password email

--- AirlineSystem-ejb/src/java/CI/Session/EmailSessionBean.java
+++ AirlineSystem-ejb/src/java/CI/Session/EmailSessionBean.java
@@ -5,8 +5,11 @@
  */
 package CI.Session;
 
+import CI.Entity.Employee;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
+import java.util.Random;
 import javax.ejb.Stateless;
 import javax.mail.Authenticator;
 import javax.mail.Message;
@@ -16,6 +19,10 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -23,6 +30,9 @@ import javax.mail.internet.MimeMessage;
  */
 @Stateless
 public class EmailSessionBean implements EmailSessionBeanLocal {
+
+    @PersistenceContext(unitName = "AirlineSystem-ejbPU")
+    private EntityManager em;
 
     private int port = 465;
     private String host = "smtp.gmail.com";
@@ -76,6 +86,54 @@ public class EmailSessionBean implements EmailSessionBeanLocal {
             ex.printStackTrace();
         }
 
+    }
+
+    @Override
+    public String validateUser(String userName, String NRIC) {
+        Employee employee = getEmployee(userName);
+        String email;
+        if (employee != null) {
+            if (employee.getEmployeeID().equals(NRIC)) {
+                email = employee.getEmployeeEmailAddress(); //need to change 
+            } else {
+                email = "";
+            }
+        } else {
+            email = "";
+        }
+        return email;
+    }
+
+    @Override
+    public String passGen() {
+        String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random rdm = new Random();
+        StringBuilder sb = new StringBuilder(6);
+        for (int i = 0; i < 6; i++) {
+            sb.append(AB.charAt(rdm.nextInt(AB.length())));
+        }
+        return sb.toString();
+    }
+
+    public Employee getEmployee(String employeeUserName) {
+        Employee employee = new Employee();
+        try {
+
+            Query q = em.createQuery("SELECT a FROM Employee " + "AS a WHERE a.employeeUserName=:userName");
+            q.setParameter("userName", employeeUserName);
+
+            List results = q.getResultList();
+            if (!results.isEmpty()) {
+                employee = (Employee) results.get(0);
+
+            } else {
+                employee = null;
+            }
+
+        } catch (EntityNotFoundException enfe) {
+            System.out.println("\nEntity not found error" + "enfe.getMessage()");
+        }
+        return employee;
     }
 
 }
