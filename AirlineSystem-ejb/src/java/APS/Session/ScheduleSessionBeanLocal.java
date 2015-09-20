8cd848edf75d9ab5e001eb9fa80379cc7896e07a
word3play <word3play@hotmail.com>
2015-09-20 15:42:52 +0800

    Added Schedule session bean

---
+++ AirlineSystem-ejb/src/java/APS/Session/ScheduleSessionBeanLocal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package APS.Session;
+
+import APS.Entity.Flight;
+import APS.Entity.Schedule;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Yanlong
+ */
+@Local
+public interface ScheduleSessionBeanLocal {
+
+    public void addSchedule(Date startDate, String flightNo);
+    public void deleteSchedule(Long id);
+    public Schedule getSchedule(Long id);
+    public List<Schedule> getSchedules();
+}
