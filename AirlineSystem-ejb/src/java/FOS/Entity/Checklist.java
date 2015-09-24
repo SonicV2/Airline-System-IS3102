0b19db0c7b3b397c7b25f9a2ad39b0e099abc01b
Yunna <Yunna@192.168.2.11>
2015-09-24 16:32:56 +0800

    minor edits on APS interfaces & update status of spoilt aircraft

--- AirlineSystem-ejb/src/java/FOS/Entity/Checklist.java
+++ AirlineSystem-ejb/src/java/FOS/Entity/Checklist.java
@@ -25,8 +25,9 @@ public class Checklist implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String name;
+    private String comments;
     
-    public String name;
     @OneToMany(cascade = {CascadeType.PERSIST})
     private List<ChecklistItem> checklistItems = new ArrayList<ChecklistItem>();
     
@@ -53,13 +54,34 @@ public class Checklist implements Serializable {
     public void setChecklistItems(List<ChecklistItem> checklistItems) {
         this.checklistItems = checklistItems;
     }
-    
+
+  
     public void addChecklistItem (ChecklistItem checklistItem){
         checklistItems.add(checklistItem);
     }
-
-   
     
+     public void removeChecklistItem (ChecklistItem item){
+        checklistItems.remove(item);
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }   
+    
+    public void checkItemAsCompleted(ChecklistItem item){
+        long id = item.getId();
+        for (ChecklistItem eachItem: checklistItems){
+            if (eachItem.getId()==id)
+                eachItem.setChecked(true);
+        }
+        
+    }
+    
+
     @Override
     public String toString() {
         return "FOS.Entity.Checklist[ id=" + id + " ]";
