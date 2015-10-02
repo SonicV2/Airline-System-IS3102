0c856824592af01a2a9a5c93c02462937d707903
Yunna <Yunna@r-32-105-25-172.comp.nus.edu.sg>
2015-10-02 14:53:38 +0800

    baggage and booking entities

--- AirlineSystem-ejb/src/java/Inventory/Entity/Booking.java
+++ AirlineSystem-ejb/src/java/Inventory/Entity/Booking.java
@@ -6,6 +6,7 @@
 package Inventory.Entity;
 
 import APS.Entity.Flight;
+import Distribution.Entity.Baggage;
 import java.io.Serializable;
 import java.util.ArrayList;
 import javax.persistence.Entity;
@@ -15,6 +16,7 @@ import javax.persistence.Id;
 import java.util.*;
 import javax.persistence.CascadeType;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -39,7 +41,25 @@ public class Booking implements Serializable {
     @Temporal(TemporalType.TIMESTAMP)
     private Date flightDate;
     private String bookingStatus;
-
+    
+    private String travellerTitle;
+    private String travellerFristName;
+    private String travellerLastName;
+    private String passportNumber;
+    private String nationality;
+    private String customerId;
+    
+    private boolean isChild;
+    private boolean boughtInsurance;
+    private double insuranceFare;
+    private String foodSelection;
+    
+    @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "booking")
+    private List<Baggage> baggages = new ArrayList<Baggage>();
+    
+    private double totalWeightAllowed;
+    private int totalNumBaggeAlloewd;
+    
     
     public void createBooking(double price, String serviceType, 
             SeatAvailability seatAvail){
@@ -163,6 +183,111 @@ public class Booking implements Serializable {
     public void setBookingStatus(String bookingStatus) {
         this.bookingStatus = bookingStatus;
     }
+
+    public String getTravellerTitle() {
+        return travellerTitle;
+    }
+
+    public String getTravellerFristName() {
+        return travellerFristName;
+    }
+
+    public String getTravellerLastName() {
+        return travellerLastName;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public boolean isIsChild() {
+        return isChild;
+    }
+
+    public boolean isBoughtInsurance() {
+        return boughtInsurance;
+    }
+
+    public double getInsuranceFare() {
+        return insuranceFare;
+    }
+
+    public String getFoodSelection() {
+        return foodSelection;
+    }
+
+    public List<Baggage> getBaggages() {
+        return baggages;
+    }
+
+    public double getTotalWeightAllowed() {
+        return totalWeightAllowed;
+    }
+
+    public int getTotalNumBaggeAlloewd() {
+        return totalNumBaggeAlloewd;
+    }
+
+    public void setTravellerTitle(String travellerTitle) {
+        this.travellerTitle = travellerTitle;
+    }
+
+    public void setTravellerFristName(String travellerFristName) {
+        this.travellerFristName = travellerFristName;
+    }
+
+    public void setTravellerLastName(String travellerLastName) {
+        this.travellerLastName = travellerLastName;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setIsChild(boolean isChild) {
+        this.isChild = isChild;
+    }
+
+    public void setBoughtInsurance(boolean boughtInsurance) {
+        this.boughtInsurance = boughtInsurance;
+    }
+
+    public void setInsuranceFare(double insuranceFare) {
+        this.insuranceFare = insuranceFare;
+    }
+
+    public void setFoodSelection(String foodSelection) {
+        this.foodSelection = foodSelection;
+    }
+
+    public void setBaggages(List<Baggage> baggages) {
+        this.baggages = baggages;
+    }
+
+    public void setTotalWeightAllowed(double totalWeightAllowed) {
+        this.totalWeightAllowed = totalWeightAllowed;
+    }
+
+    public void setTotalNumBaggeAlloewd(int totalNumBaggeAlloewd) {
+        this.totalNumBaggeAlloewd = totalNumBaggeAlloewd;
+    }
    
+    
     
 }
