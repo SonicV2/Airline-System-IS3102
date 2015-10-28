001444add209647c76314f522910d9570a6170b0
YiQuan <YiQuan@r-147-103-25-172.comp.nus.edu.sg>
2015-10-28 14:50:20 +0800

    Merge origin/master
    Conflicts:
    	AirlineSystem-war/src/java/Inventory/Managedbean/ClassManageBean.java
    	AirlineSystem-war/web/CRM/ViewListResults.xhtml
    	AirlineSystem-war/web/Inventory/CreateBookingClass.xhtml
    	AirlineSystem-war/web/Inventory/DisplayBookingClasses.xhtml

---
+++ AirlineSystem-war/src/java/Inventory/Managedbean/FlightDataManageBean.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Inventory.Managedbean;
+
+import javax.inject.Named;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import Inventory.Session.BookingSessionBeanLocal;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author YiQuan
+ */
+@Named(value = "flightDataManageBean")
+@ManagedBean
+@SessionScoped
+public class FlightDataManageBean {
+     @EJB
+    private BookingSessionBeanLocal bs;
+     
+     
+
+    /**
+     * Creates a new instance of flightDataManageBean
+     */
+    public FlightDataManageBean() {
+    }
+    
+   
+    
+    
+    private String flightNo;
+    
+    public  void createData(){
+        bs.bookSeats(flightNo);
+    }
+
+    public String getFlightNo() {
+        return flightNo;
+    }
+
+    public void setFlightNo(String flightNo) {
+        this.flightNo = flightNo;
+    }
+    
+}
